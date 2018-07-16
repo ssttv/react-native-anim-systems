ad09bc133172f8992690fe9216242cd2aeee07ff
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-07-16 21:04:13 +0300

    chore: fix stylesheet declaration

--- App.js
+++ App.js
@@ -2,6 +2,13 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Ball from "./src/Ball";
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff"
+  }
+});
+
 export default class App extends React.Component {
   render() {
     return (
@@ -11,10 +18,3 @@ export default class App extends React.Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff"
-  }
-});
