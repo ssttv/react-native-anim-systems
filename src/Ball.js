d6192d271374e93d29e773f748ff7e929a55bb9a
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-07-16 20:56:42 +0300

    feat: animate the Ball

--- src/Ball.js
+++ src/Ball.js
@@ -1,11 +1,5 @@
 import React, { Component } from "react";
-import { View } from "react-native";
-
-class Ball extends Component {
-  render() {
-    return <View style={styles.ball} />;
-  }
-}
+import { View, Animated } from "react-native";
 
 const styles = StyleSheet.create({
   ball: {
@@ -16,5 +10,22 @@ const styles = StyleSheet.create({
     borderColor: "black"
   }
 });
+
+class Ball extends Component {
+  componentWillMount() {
+    this.position = new Animated.ValueXY(0, 0);
+    Animated.spring(this.position, {
+      toValue: { x: 200, y: 500 }
+    }).start();
+  }
+
+  render() {
+    return (
+      <Animated.View style={this.position.getLayout()}>
+        <View style={styles.ball} />;
+      </Animated.View>
+    );
+  }
+}
 
 export default Ball;
